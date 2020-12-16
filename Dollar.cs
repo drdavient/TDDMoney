526a95a3cff3d6ff8e78cf971f62ddf27b6bab8b
drdavient <dave@drdavient.com>
2020-12-16 14:37:12 +0000

    Initial Commit + Preparing to test equality

---
+++ Dollar.cs
@@ -0,0 +1,25 @@
+﻿namespace TDD_Money
+{
+    public struct Dollar
+    {
+        public int amount;
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Dollar Times(int multiplier)
+        {
+            return new Dollar(amount * multiplier);
+            
+        }
+    }
+    
+    // todo $5 +10 CHF = $10 if rate is 2:1
+    // done $5 * 2 = $10
+    // todo Make amount private
+    // done Remove Dollar side effects
+    // todo Money rounding
+    // todo equals
+    // todo hashcode
+}
---
+++ TestMoney/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using TDD_Money;
+
+namespace TestMoney
+{
+    public class Tests
+    {
+        [Fact]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            Dollar result = five.Times(2);
+            Assert.True(10 == result.amount);
+            result = five.Times(3);
+            Assert.True(15 == result.amount);
+        }
+
+        [Fact]
+        public void TestEquality()
+        {
+            
+        }
+    }
+}
