70c4beb2875f35680a3f70d0bc9850b19925c6ef
drdavient <dave@drdavient.com>
2021-01-02 14:31:18 +0000

    The Root of all Evil Chapter 11

--- TestMoney/Tests.cs
+++ TestMoney/Tests.cs
@@ -17,7 +17,7 @@ namespace TestMoney
         [Fact]
         public void TestFrancMultiplication()
         {
-            Franc five = Money.Franc(5);
+            Money five = Money.Franc(5);
             Assert.True(Money.Franc(10).Equals( five.Times(2)));
             Assert.True(Money.Franc(15).Equals( five.Times(3)));
         }
@@ -25,11 +25,8 @@ namespace TestMoney
         [Fact]
         public void TestEquality()
         {
-            
             Assert.Equal(Money.Dollar(5), Money.Dollar(5));
             Assert.NotEqual(Money.Dollar(5), Money.Dollar(6));
-            Assert.Equal(Money.Franc(5),  Money.Franc (5));
-            Assert.NotEqual(Money.Franc(5), Money.Franc(6));
             Assert.NotEqual((Money) Money.Franc(5), (Money) Money.Dollar(5));
         }
 
@@ -38,18 +35,6 @@ namespace TestMoney
         {
             Assert.Equal("USD", Money.Dollar(1).Currency());
             Assert.Equal("CHF", Money.Franc(1).Currency());
-        }
-
-        [Fact]
-        public void TestDifferentClassEquality()
-        {
-            Assert.True(new Money(10,"CHF").Equals(new Franc(10,"CHF")));
-        }
-        
-        [Fact]
-        public void TestSameCurrencyEquality()
-        {
-            Assert.Equal(new Money(10,"CHF").Currency(), new Franc(10,"CHF").Currency());
         }
     }
 }
